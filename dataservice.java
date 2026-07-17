2b81deed11a8b5d64437c98d084476701455b230
Nicht Jens <166851773+KastusDiea@users.noreply.github.com>
2026-07-17 16:09:29 +0200

    Add CRUD methods for CreatorData management

--- dataservice.java
+++ dataservice.java
@@ -1,4 +1,14 @@
 public class dataservice{
 
+  public void createCreatorData(CreatorData _d){}
+
+  public void refreshCreatorData(CreatorName _c){}
+
+  public void deleteCreatorData(CreatorName _c){}
+
+  public void deleteAllData(){}
+
+  public 
+
 
 }
