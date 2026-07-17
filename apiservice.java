b75c3e25efecfad634cad1bc950ccd36c1a642b8
Nicht Jens <166851773+KastusDiea@users.noreply.github.com>
2026-07-17 15:36:14 +0200

    Refactor apiservice and add shootQuery method
    Refactor apiservice to use CreatorName and PlattformName types. Add shootQuery method with comments for future implementation.

--- apiservice.java
+++ apiservice.java
@@ -1,4 +1,15 @@
+import dataservice;
+
 public class apiservice{
-  private String creatorname;
-  private String platform;
+  private CreatorName creatorname;
+  private PlattformName platform;
+
+  public QueryResult shootQuery( CreatorName _c, PlattformName _p){
+    //check dataservice for existing info with CreatorData dataservice.getIntel() (throws error if null)
+      //if no info, get it from API of _p (possible: YouTube, Instagram, TikTok) with  String _p.getPlattformName()
+      //save it to dataservice with dataservice.storeIntel(CreatorData _d)
+      //build QueryResult from CreatorData object
+    //filter Queryresult with filter()
+    //return QueryResult
+  }
 }
