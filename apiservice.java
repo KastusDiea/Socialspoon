ebde252a050c5fe083985993394f612ad39ceae9
Nicht Jens <166851773+KastusDiea@users.noreply.github.com>
2026-07-20 18:13:39 +0200

    Refactor shootQuery to improve data retrieval logic
    Refactor shootQuery method to retrieve and store CreatorData from API if not available in dataservice.

--- apiservice.java
+++ apiservice.java
@@ -5,13 +5,15 @@ public class apiservice{
   private CreatorName creatorname;
   private PlattformName platform;
 
-  public QueryResult shootQuery( CreatorName _c, PlattformName _p){
-    //check dataservice for existing info with CreatorData dataservice.getIntel() (throws error if null)
-      //if no info, get it from API of _p (possible: YouTube, Instagram, TikTok) with  String _p.getPlattformName()
-      //save it to dataservice with dataservice.storeIntel(CreatorData _d)
-      //build QueryResult from CreatorData object
-    //filter Queryresult with filter()
-    //return QueryResult
+  public QueryResult shootQuery(CreatorName creator, PlattformName platform) {
+    CreatorData data = dataservice.getIntel(creator, platform);
+    if (data == null) {
+        data = ApiFactory
+                .getApi(platform)
+                .loadCreatorData(creator);
+        dataservice.storeIntel(data);
+    }
+    return filter(data);
   }
 
   public QueryResult filter(CreatorData){
