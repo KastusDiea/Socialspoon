de1cb2deee2637448770e36fb11fc04f6bb4da8a
Nicht Jens <166851773+KastusDiea@users.noreply.github.com>
2026-08-11 18:44:57 +0200

    Remove filter and sort methods from UiService
    Removed filter and sort methods from UiService.

--- UiService.py
+++ UiService.py
@@ -46,37 +46,3 @@ class UIService:
         Prints the entire table.
         """
         print(self.get_table())
-
-
-    def filter(self, **kwargs):
-        """
-        Example:
-            filter(Platform="YouTube")
-            filter(Creator="MrBeast")
-            filter(Subscribers=1000000)
-        """
-
-        df = self.get_table()
-
-        for column, value in kwargs.items():
-            if column not in df.columns:
-                raise ValueError(f"'{column}' is not in the Database.")
-
-            df = df[df[column] == value]
-
-        return df
-
-
-    def sort(self, by, ascending=False):
-        """
-        Example:
-            sort("Views")
-            sort("Subscribers")
-        """
-
-        df = self.get_table()
-
-        if by not in df.columns:
-            raise ValueError(f"'{by}' is not possible to sort")
-
-        return df.sort_values(by=by, ascending=ascending)
