6a91ac92582bdc2d06114e54c3da8f5ea8d663bc
Nicht Jens <166851773+KastusDiea@users.noreply.github.com>
2026-08-11 18:48:46 +0200

    Implement DbPullService to retrieve creator data

--- dbPullService.java
+++ dbPullService.java
@@ -1,1 +1,39 @@
+class DbPullService:
+
+    def __init__(self, creator_database):
+        self.creator_database = creator_database
+
+
+    def get_table(self):
+
+        rows = []
+
+        for creator in self.creator_database:
+
+            platform = creator.platform.get_name()
+
+            if len(creator.videos) == 0:
+                rows.append({
+                    "Platform": platform,
+                    "Creator": creator.name,
+                    "Subscribers": creator.subscribers,
+                    "Title": "",
+                    "Views": None,
+                    "Likes": None,
+                    "Upload Date": ""
+                })
+
+            else:
+                for video in creator.videos:
+                    rows.append({
+                        "Platform": platform,
+                        "Creator": creator.name,
+                        "Subscribers": creator.subscribers,
+                        "Title": video.title,
+                        "Views": video.views,
+                        "Likes": video.likes,
+                        "Upload Date": video.upload_date
+                    })
+
+        return pd.DataFrame(rows)
 
